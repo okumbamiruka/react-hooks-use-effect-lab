362f130c6c450d1df66532aa372d4347b9d4ed44
Chrispine Oburu <chrizoburu65@gmail.com>
2024-04-29 16:21:36 +0300

    use effect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // Setup timer interval
+    const timerInterval = setInterval(() => {
+      // Decrease time remaining by 1 second
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    // Cleanup interval on component unmount or when time runs out
+    return () => clearInterval(timerInterval);
+  }, [timeRemaining, onAnswered]); // Empty dependency array ensures the effect runs only once on component mount
+
+  useEffect(() => {
+    // Check if time runs out
+    if (timeRemaining === 0) {
+      // Call onAnswered with false (indicating time's up)
+      handleAnswer(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
+    // Reset timer when answering
     setTimeRemaining(10);
+    // Call parent component's callback
     onAnswered(isCorrect);
   }
 
